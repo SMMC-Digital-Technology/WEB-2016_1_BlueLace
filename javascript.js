ff9275d0aec61b5acd5d180783bd49b04ebb8671
SMMC-Web-Design <alan.burt@mackillop.act.edu.au>
2016-06-01 14:06:41 +1000

    This is the hopefully final bit
    This is the hopefully final bit

--- javascript.js
+++ javascript.js
@@ -26,3 +26,47 @@ function calculator() {
     var output = document.getElementById("output");
     output.value = tax;
 }
+
+function drawGraph() {
+
+    
+    var trace, data, layout, xData = [], yData = [];
+    
+    var x = 250000 / 1000;
+    for(var i = 0; i < x; i++) {
+        xData[i] = i * 1000;
+        yData[i] = calculate(xData[i]);
+    }
+
+
+    trace = {
+        x: xData, // x values
+        y: yData, // y values
+        type: 'scatter', // the type of graph
+        mode: 'lines+markers', // how the data should be displayed
+        name: "trace" // a name for the data to appear on the legend (if present)
+    };
+
+    // Data is an array that stores the traces we wish to graph. Add further traces to the array for multiple data sets.
+    data = [trace];
+
+    /* To add a title and axis labels, we need to create a layout (another object). Setting a layout is optional, but they do improve the graph. */
+    layout = {
+        title: "A simple graph in Plotly",
+        yaxis: {
+            title: "y-axis", // give the axis a label
+            zerolinewidth: 1.5 // makes the zero line thicker
+        },
+        xaxis: {
+            title: "x-axis",
+            zerolinewidth: 1.5,
+            range: [0, 5] // sets a minimum and maximum value for the axis
+        }
+    };
+
+    /* Finally we tell plotly to put the graph in the 'graph' div.*/
+    Plotly.newPlot("graph", data, layout);
+}
+
+// Run the function to draw the graph.
+drawGraph();
